10fdd4fe1ba6c6b92d0a81d49d75f57c7dffcee0
KattyDan <danilovae235@gmail.com>
2025-05-02 13:21:15 +0300

    Update задание1.py

--- задание1.py
+++ задание1.py
@@ -1,8 +1,8 @@
 a=input('nepвoe чиcлo') 
-b= input ('второе число') 
+b= input ('второе число') #Ввод данных
 c=input ("третье число") 
-if a==b==c: 
+if a==b==c: #Проверка на равнснтво 
 	print('равны')
-ma=max(a,b,c)
+ma=max(a,b,c) #Поиск max и min
 mi=min(a,b,c)
 print(ma, mi)
